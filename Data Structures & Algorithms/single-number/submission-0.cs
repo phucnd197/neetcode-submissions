fc277b2aa81bf841f8db5c1529bafdd24132e361
phucnd197 <92969692+phucnd197@users.noreply.github.com>
2026-06-03 08:25:37 +0700

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        var result = 0; 
+        for(var i = 0; i < nums.Length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
