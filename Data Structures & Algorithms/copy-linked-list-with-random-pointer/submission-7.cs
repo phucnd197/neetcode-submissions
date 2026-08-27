66c2e158cad60067a2d9d6adc427ca3279ff08a9
phucnd197 <92969692+phucnd197@users.noreply.github.com>
2026-08-27 17:44:59 +0700

    Add: copy-linked-list-with-random-pointer - submission-7

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-7.cs
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val) {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+*/
+
+public class Solution {
+    public Node copyRandomList(Node head) {
+        if (head is null) {
+            return head;
+        }
+
+        var curr = head;
+        while (curr is not null) {
+            var oldNext = curr.next;
+            var newNode = new Node(curr.val) { next = oldNext };
+            curr.next = newNode;
+            curr = oldNext;
+        }
+
+        curr = head;
+        while (curr is not null) {
+            if (curr.random is not null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next?.next;
+        }
+
+        var newHead = head.next;
+        var currNew = newHead;
+        curr = head;
+        while (currNew is not null) {
+            var oldNext = curr.next?.next;
+            var newNext = currNew.next?.next;
+            curr.next = oldNext;
+            currNew.next = newNext;
+            curr = oldNext;
+            currNew = newNext;
+        }
+
+        return newHead;
+    }
+}
